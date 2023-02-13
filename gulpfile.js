007bed4185b8d9aa5421ee6104789b999edda2f4
Albert <albertsoares97@gmail.com>
2023-02-13 20:28:46 -0300

    criando o hero, importando a fonte Avenir e ajuste

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'))
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss') // apenas recuperamos os arquivos, ainda não estamos compilando o sass
@@ -7,7 +8,13 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*') // apenas recuperamos os arquivos, ainda não estamos compilando o sass
+        .pipe(imagemin()) // compilando o sass
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
